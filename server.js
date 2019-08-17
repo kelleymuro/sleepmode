2ebc63b18ec82361fa8527ea88c8cbf1f6daf167
Kelley Muro <kelleymuro@gmail.com>
2019-08-16 20:13:42 -0700

    Load the app

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 
 connectDB();
 
+// Init Middleware
 app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
@@ -16,7 +17,7 @@ if(process.env.NODE_ENV === 'production') {
    app.use(express.static('client/build'));
 
    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    });
 }
 
