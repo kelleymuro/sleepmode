551f4229dd0bf54eaf55132c1ac7eed839f9c6bf
Kelley Muro <kelleymuro@gmail.com>
2019-08-16 20:00:30 -0700

    added required build scripts

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB  = require('./config/db');
-
-// For Testing
-app.get('/', (req, res) => res.send('API Running'))
+const path = require('path');
 
 connectDB();
 
@@ -11,6 +9,16 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+   // Set static folder
+   app.use(express.static('client/build'));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+   });
+}
 
 const PORT = process.env.PORT || 5000;
 
